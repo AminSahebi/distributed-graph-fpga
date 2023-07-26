4ef5ce53900118c6c769911b1092d4f370cdf916
amin <sahebi.amin@gmail.com>
2023-07-26 09:14:40 +0100

    pagerank with stream

--- hw_src/FPGA/kernel/pagerank/pagerank_kernel.cpp
+++ hw_src/FPGA/kernel/pagerank/pagerank_kernel.cpp
@@ -1,159 +1,147 @@
-/*
-   Copyright (c) 2020-2021 Amin Sahebi, 
-   University of Siena, University of Florence.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 #include <ap_int.h>
 #include <stdint.h>
 #include <string.h>
-//#include "defn.h"
+#include <hls_stream.h>
 
 #define DAMPING_FACTOR 	0.85
-//#define BUFFER_SIZE 	512
-#define DATA_WIDTH 	512
-#define PE 	2	
-
-#define BUF_PER_PE	64//BUFFER_SIZE/PE
+#define DATA_WIDTH 	    512
+#define PE 	            1	
+#define BUF_PER_PE	    64   // Reduced buffer size per PE
 
 typedef ap_uint<DATA_WIDTH> u_data;
-//typedef ap_uint<64> u32;
 
 typedef unsigned int u32;
-typedef unsigned int u16;
 
-void PE_kernel(u32 local_in_a[], u32 local_in_b[], u32 local_in_c[], u32 local_out[], int v) {
-
-	u32 prev_buffer[BUF_PER_PE]; // Local Memory to store result
+void PE_kernel(u_data local_in_a[], u_data local_in_b[], u_data local_in_c[], u_data local_out[], int v) {
+    // The implementation of PE_kernel remains unchanged
+    // ... (as in your original code) ...
+#pragma HLS inline
+        u32 prev_buffer[BUF_PER_PE]; // Local Memory to store result
 #pragma HLS ARRAY_PARTITION variable=prev_buffer dim=0
-	u32 rank_buffer[BUF_PER_PE]; // Local Memory to store result
+        u32 rank_buffer[BUF_PER_PE]; // Local Memory to store result
 #pragma HLS ARRAY_PARTITION variable=rank_buffer dim=0
-	// start init grid algorithm and
-	// init "rank" and "prev" memories.
+        // start init grid algorithm and
+        // init "rank" and "prev" memories.
 
-	float adding_constant = (1 - DAMPING_FACTOR) * 1/(float)v;
-	float one_over_n = 1.0/(float)v;
+        float adding_constant = (1 - DAMPING_FACTOR) * 1/(float)v;
+        float one_over_n = 1.0/(float)v;
 
-loop_initialization:	for(int j = 0; j < BUF_PER_PE; j++){
-#pragma HLS pipeline
-				prev_buffer[j] = one_over_n;
-				rank_buffer[j] = 0.0;
-			}
+loop_initialization:    for(int j = 0; j < BUF_PER_PE; j++){
+#pragma HLS pipeline II=1
+                                prev_buffer[j] = one_over_n;
+                                rank_buffer[j] = 0.0;
+                        }
 
 
-kernel_loop:		for(int j = 0; j < BUF_PER_PE; j++){
-#pragma HLS pipeline //II=1
+kernel_loop:            for(int j = 0; j < BUF_PER_PE; j++){
+#pragma HLS pipeline II=1
+                                u32 src = local_in_a[j]; //edge src buffer
+                                u32 dst = local_in_b[j]; //edge dst buffer
+                                u32 deg = local_in_c[j]; //outdegree buffer
+                                u32 temp = prev_buffer[src]/deg;
+                                rank_buffer[dst] = temp;
+                                local_out[j] = rank_buffer[dst];
+                        }
 
-				u32 src = local_in_a[j]; //edge src buffer
-				u32 dst = local_in_b[j]; //edge dst buffer
-				u32 deg = local_in_c[j]; //outdegree buffer
-				u32 temp = prev_buffer[src]/deg;
-				rank_buffer[dst] = temp;
-				local_out[j] = rank_buffer[dst];
-			}
 }
 
+void scatter_process(
+    u_data *e_src,
+    u_data *e_dst,
+    u_data *out_degree,
+    hls::stream<u_data> src_stream[PE],
+    hls::stream<u_data> dst_stream[PE],
+    hls::stream<u_data> deg_stream[PE],
+    int size,
+    int vertices
+) {
+    int v = vertices;
 
-void buffer_load(u32 local_in_a[PE][BUF_PER_PE], u32 local_in_b[PE][BUF_PER_PE], u32 local_in_c[PE][BUF_PER_PE], u_data *global_in_a, u_data *global_in_b, u_data *global_in_c) {
-	// burst read
-load_loop:	for(int i = 0; i < PE; i++){
-#pragma HLS unroll
-			memcpy(local_in_a[i], &global_in_a[i * BUF_PER_PE], BUF_PER_PE * sizeof(u32));
-			memcpy(local_in_b[i], &global_in_b[i * BUF_PER_PE], BUF_PER_PE * sizeof(u32));
-			memcpy(local_in_c[i], &global_in_c[i * BUF_PER_PE], BUF_PER_PE * sizeof(u32));
-		}
+scatter_loop:
+    for (int i = 0; i < size; i++) {
+        #pragma HLS pipeline II=1
+        int pe_id = i % PE; // Partition data across PEs
+        src_stream[pe_id].write(e_src[i]);
+        dst_stream[pe_id].write(e_dst[i]);
+        deg_stream[pe_id].write(out_degree[i]);
+    }
 }
 
-void buffer_compute(u32 local_in_a[PE][BUF_PER_PE], u32 local_in_b[PE][BUF_PER_PE], u32 local_in_c[PE][BUF_PER_PE], u32 local_out[PE][BUF_PER_PE], int v) {
-	// kernel replication
-compute_loop:	for (int i=0; i < PE; i++) {
-#pragma HLS unroll
-			PE_kernel(local_in_a[i], local_in_b[i], local_in_c[i], local_out[i], v);
-		}
+void apply_process(
+    hls::stream<u_data> src_stream[PE],
+    hls::stream<u_data> dst_stream[PE],
+    hls::stream<u_data> deg_stream[PE],
+    hls::stream<u_data> result_stream[PE],
+    int v
+) {
+    u_data src_buffer[PE][BUF_PER_PE];
+    u_data dst_buffer[PE][BUF_PER_PE];
+    u_data deg_buffer[PE][BUF_PER_PE];
+    u_data local_out[PE][BUF_PER_PE];
+
+apply_loop:
+    for (int i = 0; i < BUF_PER_PE; i++) {
+        #pragma HLS unroll
+        for (int pe_id = 0; pe_id < PE; pe_id++) {
+            #pragma HLS pipeline II=1
+            src_buffer[pe_id][i] = src_stream[pe_id].read();
+            dst_buffer[pe_id][i] = dst_stream[pe_id].read();
+            deg_buffer[pe_id][i] = deg_stream[pe_id].read();
+            PE_kernel(&src_buffer[pe_id][i], &dst_buffer[pe_id][i], &deg_buffer[pe_id][i], &local_out[pe_id][i], v);
+        }
+    }
+
+apply_write:
+    for (int i = 0; i < BUF_PER_PE; i++) {
+        #pragma HLS unroll
+        for (int pe_id = 0; pe_id < PE; pe_id++) {
+            #pragma HLS pipeline II=1
+            result_stream[pe_id].write(local_out[pe_id][i]);
+        }
+    }
 }
 
-void buffer_store(u_data *global_out, u32 local_out[PE][BUF_PER_PE]) {
-store_loop:	for(int i = 0; i < PE; i++) {
-#pragma HLS unroll
-			memcpy(&global_out[i * BUF_PER_PE], local_out[i], BUF_PER_PE * sizeof(u32));
-		}
+void reduce_process(
+    hls::stream<u_data> result_stream[PE],
+    u_data *out_r,
+    int size
+) {
+reduce_loop:
+    for (int i = 0; i < size; i++) {
+        #pragma HLS pipeline II=1
+        for (int pe_id = 0; pe_id < PE; pe_id++) {
+            out_r[i * PE + pe_id] = result_stream[pe_id].read();
+        }
+    }
 }
 
 extern "C" {
-	void pagerank_kernel_0(
-			u_data *e_src, 		// edge sources
-			u_data *e_dst, 		// edge destinations
-			u_data *out_degree, 	// out_degrees
-			u_data *out_r,     	// output Result
-			int size,               // size of each edge block
-			int vertices		// number of vertices
-			) {
-#pragma HLS INTERFACE m_axi port = e_src offset = slave bundle=gmem num_write_outstanding=64 max_write_burst_length=64 num_read_outstanding=64 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = e_dst offset = slave bundle=gmem num_write_outstanding=64 max_write_burst_length=64 num_read_outstanding=64 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = out_degree offset = slave bundle=gmem num_write_outstanding=64 max_write_burst_length=64 num_read_outstanding=64 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = out_r offset = slave bundle=gmem num_write_outstanding=64 max_write_burst_length=64 num_read_outstanding=64 max_read_burst_length=64
-		int v = vertices;
-		//create local buffers
-		u32 e_src_buffer_a[PE][BUF_PER_PE];   // Local memory to store edge source
-#pragma HLS ARRAY_PARTITION variable=e_src_buffer_a dim=1 complete 
+    void pagerank_kernel_0(
+            u_data *e_src,
+            u_data *e_dst,
+            u_data *out_degree,
+            u_data *out_r,
+            int size,
+            int vertices
+    ) {
+        hls::stream<u_data> src_stream[PE];
+        hls::stream<u_data> dst_stream[PE];
+        hls::stream<u_data> deg_stream[PE];
+        hls::stream<u_data> result_stream[PE];
 
-		u32 e_src_buffer_b[PE][BUF_PER_PE];   // Local memory to store edge source
-#pragma HLS ARRAY_PARTITION variable=e_src_buffer_b dim=1 complete 
+    #pragma HLS dataflow
 
-		u32 e_dst_buffer_a[PE][BUF_PER_PE];   // Local memory to store edge dest
-#pragma HLS ARRAY_PARTITION variable=e_dst_buffer_a dim=1 complete
+        int v = vertices;
 
-		u32 e_dst_buffer_b[PE][BUF_PER_PE];   // Local memory to store edge dest
-#pragma HLS ARRAY_PARTITION variable=e_dst_buffer_b dim=1 complete
+        // Set depth of streams using pragma
+        #pragma HLS stream variable=src_stream depth=8//BUF_PER_PE
+        #pragma HLS stream variable=dst_stream depth=8//BUF_PER_PE
+        #pragma HLS stream variable=deg_stream depth=8//BUF_PER_PE
+        #pragma HLS stream variable=result_stream depth=8//BUF_PER_PE
 
-		u32 out_deg_buffer_a[PE][BUF_PER_PE]; // Local Memory to store out degree
-#pragma HLS ARRAY_PARTITION variable=out_deg_buffer_a dim=1 complete
-
-		u32 out_deg_buffer_b[PE][BUF_PER_PE]; // Local Memory to store out degree
-#pragma HLS ARRAY_PARTITION variable=out_deg_buffer_b dim=1 complete
-
-		u32 output_buffer_a[PE][BUF_PER_PE]; // Local Memory to store result
-#pragma HLS ARRAY_PARTITION variable=output_buffer_a dim=1 complete
-
-		u32 output_buffer_b[PE][BUF_PER_PE]; // Local Memory to store result
-#pragma HLS ARRAY_PARTITION variable=output_buffer_b dim=1 complete
-
-		u32 local_out[PE][BUF_PER_PE]; // Local temp for kernel output
-#pragma HLS ARRAY_PARTITION variable=local_out dim=1 complete
-
-		u32 local_in_a[PE][BUF_PER_PE]; // Local temp for kernel computation
-#pragma HLS ARRAY_PARTITION variable=local_in_a dim=1 complete
-
-		u32 local_in_b[PE][BUF_PER_PE]; // Local temp for kernel computation
-#pragma HLS ARRAY_PARTITION variable=local_in_b dim=1 complete
-
-		u32 local_in_c[PE][BUF_PER_PE]; // Local temp for kernel computation
-#pragma HLS ARRAY_PARTITION variable=local_in_c dim=1 complete
-
-
-start_loop:		for (int i = 0; i < size/BUF_PER_PE+1; i++) {
-			//double buffering
-		//	if(i % 2 == 0) {
-				buffer_load(e_src_buffer_a, e_dst_buffer_a, out_deg_buffer_a, &e_src[i*BUF_PER_PE], &e_dst[i*BUF_PER_PE], &out_degree[i*BUF_PER_PE]);
-				buffer_compute(e_src_buffer_b, e_dst_buffer_b, out_deg_buffer_b, output_buffer_b, v);
-				buffer_store(&out_r[i*BUF_PER_PE], output_buffer_a);
-		//	} else {
-		//		buffer_load(e_src_buffer_b, e_dst_buffer_b, out_deg_buffer_b, &e_src[i*BUF_PER_PE], &e_dst[i*BUF_PER_PE], &out_degree[i*BUF_PER_PE]);
-		//		buffer_compute(e_src_buffer_a, e_dst_buffer_a, out_deg_buffer_a, output_buffer_a, v);
-		//		buffer_store(&out_r[i*BUF_PER_PE], output_buffer_b);
-		//	}
-		}
-	}
+        scatter_process(e_src, e_dst, out_degree, src_stream, dst_stream, deg_stream, size, vertices);
+        apply_process(src_stream, dst_stream, deg_stream, result_stream, vertices);
+        reduce_process(result_stream, out_r, size);
+    }
 }
 
