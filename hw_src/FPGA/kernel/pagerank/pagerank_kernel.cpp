937905eabdf3f09f241e658bdda427f442b921ef
amin <sahebi.amin@gmail.com>
2023-07-30 18:10:49 +0100

    updates

--- hw_src/FPGA/kernel/pagerank/pagerank_kernel.cpp
+++ hw_src/FPGA/kernel/pagerank/pagerank_kernel.cpp
@@ -1,152 +1,132 @@
-/*
-   Copyright (c) 2020-2021 Amin Sahebi, 
-   University of Siena, University of Florence.
+#include <stdbool.h>
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+#define CACHE_SIZE 128 // Number of vertices to cache with the highest out-degrees
+#define DAMPING_FACTOR 0.85
+#define EPSILON 1e-6
 
-http://www.apache.org/licenses/LICENSE-2.0
+typedef unsigned int u_data;
+typedef float f_data;
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Structure to represent a cache line
+typedef struct {
+    bool valid;
+    u_data vertex;
+    f_data rank;
+} CacheLine;
 
-#include <ap_int.h>
-#include <stdint.h>
-#include <string.h>
-#include <hls_stream.h>
-
-#define DAMPING_FACTOR 	0.85
-#define DATA_WIDTH 		32
-#define PE 			2
-#define BIGN			65536 // 2^12
-#define BUF_PER_PE		128 // BUFFER_SIZE/PE
-#define CHUNK_SIZE 		16
-#define UNROLL_FACTOR 4
-
-typedef ap_uint<DATA_WIDTH> u_data;
-
-typedef unsigned int u32;
-
-void PE_kernel(u32 local_in_a[], u32 local_in_b[], u32 local_out[], int v) {
-//#pragma HLS inline
-	u32 rank_buffer[BUF_PER_PE]; // Local Memory to store result
-#pragma HLS ARRAY_PARTITION variable=rank_buffer dim=0
-
-	u32 src_buffer[BUF_PER_PE];
-#pragma HLS ARRAY_PARTITION variable=src_buffer dim=0
-	u32 dst_buffer[BUF_PER_PE];
-#pragma HLS ARRAY_PARTITION variable=dst_buffer dim=0
-
-read_buffer:	for(int j = 0; j < BUF_PER_PE; j++){
-#pragma HLS pipeline II=1
-			//#pragma HLS LOOP_FLATTEN
-			//#pragma HLS unroll
-			src_buffer[j] = local_in_a[j];
-			dst_buffer[j] = local_in_b[j];
-		}
-
-		float adding_constant = (1 - DAMPING_FACTOR) * 1/(float)v;
-		float one_over_n = 1.0/(float)v;
-
-loop_initialization:	for(int j = 0; j < BUF_PER_PE; j++){
-#pragma HLS pipeline II=1
-				rank_buffer[j] = 0.0;
-			}
-
-			/*
-kernel_loop:		for(int j = 0; j < BUF_PER_PE; j++){
-#pragma HLS LOOP_FLATTEN off
-#pragma HLS pipeline II=1
-u32 src = src_buffer[j]; //edge src buffer
-u32 deg = dst_buffer[j]; //outdegree buffer
-u32 temp = src/deg;
-u32 rank = adding_constant + DAMPING_FACTOR*temp;
-rank_buffer[j] = rank;
+// Function to initialize the cache
+void init_cache(CacheLine cache[CACHE_SIZE]) {
+    for (int i = 0; i < CACHE_SIZE; i++) {
+        cache[i].valid = false;
+        cache[i].vertex = 0;
+        cache[i].rank = 0.0;
+    }
 }
 
-*/
-kernel_loop: for (int j = 0; j < BUF_PER_PE; j += 2) {
-#pragma HLS pipeline II=1
-		     u32 src1 = src_buffer[j]; //edge src buffer
-		     u32 deg1 = dst_buffer[j]; //outdegree buffer
-		     u32 temp1 = src1 / deg1;
-		     u32 rank1 = adding_constant + DAMPING_FACTOR * temp1;
-		     rank_buffer[j] = rank1;
-
-		     u32 src2 = src_buffer[j + 1]; //edge src buffer
-		     u32 deg2 = dst_buffer[j + 1]; //outdegree buffer
-		     u32 temp2 = src2 / deg2;
-		     u32 rank2 = adding_constant + DAMPING_FACTOR * temp2;
-		     rank_buffer[j + 1] = rank2;
-	     }
-write_ranks:
-for (int j = 0; j < BUF_PER_PE; j++) {
-#pragma HLS pipeline II=1
-	//#pragma HLS pipeline
-	local_out[j] = rank_buffer[j];
+// Function to find a cache line using the vertex
+int find_cache_line(CacheLine cache[CACHE_SIZE], u_data vertex) {
+    for (int i = 0; i < CACHE_SIZE; i++) {
+        if (cache[i].valid && cache[i].vertex == vertex) {
+            return i; // Cache hit
+        }
+    }
+    return -1; // Cache miss
+}
+void load_data( u_data* e_dst, u_data* out_degree,int size, f_data out_r_local[CACHE_SIZE]) {
+	    for (int j = 0; j < size; j++) {
+		            #pragma HLS pipeline II=1
+		            u_data neighbor = e_dst[j];
+			            int out_degree_neighbor = out_degree[neighbor];
+				            if (out_degree_neighbor > 0) {
+						                out_r_local[neighbor % CACHE_SIZE] = out_degree[neighbor];
+								        }
+					        }
 }
 
+void pagerank_chunk(u_data* e_src, u_data* e_dst, u_data* out_degree, f_data* out_r, int size, int start_vertex, int chunk_size, f_data local_r[CACHE_SIZE]) {
+    //#pragma HLS inline off
+
+    for (int i = start_vertex; i < start_vertex + chunk_size; i++) {
+	            #pragma HLS loop_tripcount min=CACHE_SIZE max=CACHE_SIZE
+
+        f_data sum = 0.0;
+
+        // Cache the PageRank values of neighbor vertices in local memory
+        for (int j = 0; j < CACHE_SIZE; j++) {
+		            #pragma HLS pipeline II=1
+            local_r[j] = 0.0; // Initialize local memory
+        }
+
+        // Load the PageRank values of neighbor vertices into local memory
+        f_data out_r_local[CACHE_SIZE];
+        load_data(e_dst, out_degree, size, out_r_local);
+
+        // Compute the rank for this vertex using the cached values in local memory
+        for (int j = 0; j < CACHE_SIZE; j++) {
+		            #pragma HLS pipeline II=1
+            sum += local_r[j] / out_degree[i * CACHE_SIZE + j];
+        }
+
+        // Apply the PageRank formula for this vertex
+        f_data new_rank = (1.0 - DAMPING_FACTOR) + DAMPING_FACTOR * sum;
+
+        // Store the new rank value in the global memory
+        out_r[i] = new_rank;
+    }
 }
 
+// Function to divide the computation among PEs
+void pagerank_parallel(u_data* e_src, u_data* e_dst, u_data* out_degree, f_data* out_r, int size, int vertices) {
+    // Initialize the cache
+    CacheLine cache[CACHE_SIZE];
+    init_cache(cache);
 
+    int chunk_size = vertices / 16; // Divide the computation into 4 parts
+    int remaining_vertices = vertices;
 
-void buffer_load(u32 local_in_a[PE][BUF_PER_PE], u32 local_in_b[PE][BUF_PER_PE], u_data *global_in_a, u_data *global_in_b) {
-	// burst read
-load_loop:	for(int i = 0; i < PE; i++){
-#pragma HLS unroll 
-			memcpy(local_in_a[i], &global_in_a[i * BUF_PER_PE], BUF_PER_PE * sizeof(u32));
-			memcpy(local_in_b[i], &global_in_b[i * BUF_PER_PE], BUF_PER_PE * sizeof(u32));
-		}
-}
-
-void buffer_compute(u32 local_in_a[PE][BUF_PER_PE],u32 local_in_b[PE][BUF_PER_PE], u32 local_out[PE][BUF_PER_PE], int v) {
-	// kernel replication
-compute_loop:	for (int i=0; i < PE; i++) {
+    // Parallelize the computation for 4 PEs
+    for (int pe = 0; pe < 16; pe++) {
 #pragma HLS unroll
-			PE_kernel(local_in_a[i], local_in_b[i], local_out[i], v);
-		}
+        int start_vertex = pe * chunk_size;
+        if (pe == 15) {
+            // Last PE may have fewer vertices to process
+            chunk_size = remaining_vertices;
+        }
+        f_data local_r[CACHE_SIZE];
+#pragma HLS ARRAY_PARTITION variable=local_r dim=0 complete
+
+        pagerank_chunk(e_src, e_dst, out_degree, out_r, size, start_vertex, chunk_size, local_r);
+
+        remaining_vertices -= chunk_size;
+    }
 }
 
-void buffer_store(u_data *global_out, u32 local_out[PE][BUF_PER_PE]) {
-store_loop:	for(int i = 0; i < PE; i++) {
-#pragma HLS unroll 
-			memcpy(&global_out[i * BUF_PER_PE], local_out[i], BUF_PER_PE * sizeof(u32));
-		}
-}
+
 
 extern "C" {
-	void pagerank_kernel_0(
-			u_data *e_src, 		// edge sources
-			u_data *out_degree, 		// outdegree
-			u_data *out_r,     	// output Result
-			int size,               // size of each edge block
-			int vertices		// number of vertices
-			) {
-#pragma HLS INTERFACE m_axi port = e_src offset = slave bundle=gmem num_write_outstanding=64 max_write_burst_length=64 num_read_outstanding=64 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = out_degree offset = slave bundle=gmem num_write_outstanding=64 max_write_burst_length=64 num_read_outstanding=64 max_read_burst_length=64
-#pragma HLS INTERFACE m_axi port = out_r offset = slave bundle=gmem num_write_outstanding=64 max_write_burst_length=64 num_read_outstanding=64 max_read_burst_length=64
-		int v = vertices;
-		//create local buffers
+    void pagerank_kernel_0(
+            u_data* e_src,       // edge sources
+            u_data* e_dst,       // edge destinations
+            u_data* out_degree,  // outdegree
+            f_data* out_r,       // output Result
+            int size,            // size of each edge block
+            int vertices         // number of vertices
+        ) {
+    #pragma HLS interface m_axi port=e_src offset=slave bundle=gmem
+    #pragma HLS interface m_axi port=e_dst offset=slave bundle=gmem
+    #pragma HLS interface m_axi port=out_degree offset=slave bundle=gmem
+    #pragma HLS interface m_axi port=out_r offset=slave bundle=gmem
 
-		u32 local_out[PE][BUF_PER_PE]; // Local temp for kernel output
-#pragma HLS ARRAY_PARTITION variable=local_out dim=1 complete
+    #pragma HLS interface s_axilite port=e_src bundle=control
+    #pragma HLS interface s_axilite port=e_dst bundle=control
+    #pragma HLS interface s_axilite port=out_degree bundle=control
+    #pragma HLS interface s_axilite port=out_r bundle=control
+    #pragma HLS interface s_axilite port=size bundle=control
+    #pragma HLS interface s_axilite port=vertices bundle=control
+    #pragma HLS interface s_axilite port=return bundle=control
 
-		u32 local_in_a[PE][BUF_PER_PE]; // Local temp for kernel computation
-#pragma HLS ARRAY_PARTITION variable=local_in_a dim=1 complete
-
-		u32 local_in_b[PE][BUF_PER_PE]; // Local temp for kernel computation
-#pragma HLS ARRAY_PARTITION variable=local_in_b dim=1 complete
-
-
-start_loop:		for (int i = 0; i < size/BUF_PER_PE+1; i++) {
-				buffer_load(local_in_a, local_in_b, &e_src[i*BUF_PER_PE], &out_degree[i*BUF_PER_PE]);
-				buffer_compute(local_in_a, local_in_b, local_out, v);
-				buffer_store(&out_r[i*BUF_PER_PE], local_out);
-			}
-	}
+        // Call the parallelized function
+        pagerank_parallel(e_src, e_dst, out_degree, out_r, size, vertices);
+    }
 }
+
