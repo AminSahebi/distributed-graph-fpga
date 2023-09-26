1377a2d902cc4abeebda670ce6a4fbfc2b6dd39d
amin <sahebi.amin@gmail.com>
2023-09-26 09:19:24 +0100

    SSSP kernel modified based on commit on 23 Aug

--- hw_src/FPGA/kernel/sssp/sssp_kernel.cpp
+++ hw_src/FPGA/kernel/sssp/sssp_kernel.cpp
@@ -1,11 +1,8 @@
 #include <ap_int.h>
 #include <stdint.h>
 #include <hls_stream.h>
-#include <stdbool.h>
-#include <limits.h>
 
-#define INF UINT_MAX // Infinite distance
-
+#define INF 0xFFFFFFFF
 #define DATA_WIDTH 32
 #define PE 4
 #define BUF_PER_PE 64
@@ -24,19 +21,13 @@ struct CacheBlock {
 	CacheEntry entries[BUF_PER_PE];
 };
 
-struct Node {
-	u32 distance;
-	bool visited;
-};
-
 void update_lru(CacheBlock cache[BUF_PER_PE], int index) {
 	CacheBlock temp = cache[index];
-loop_lru:
-	for (int i = index; i > 0; i--) {
+loop_lru:	for (int i = index; i > 0; i--) {
 #pragma HLS pipeline II=1
-		cache[i] = cache[i - 1];
-	}
-	cache[0] = temp;
+			cache[i] = cache[i - 1];
+		}
+		cache[0] = temp;
 }
 
 u32 read_from_cache(CacheBlock* cache, int addr) {
@@ -60,121 +51,167 @@ void fetch_cache_block(CacheBlock* cache_L1, u_data* global_data, int addr) {
 	int block_start_addr = block_index * BUF_PER_PE;
 
 	// Unroll the inner loop to reduce loop overhead
-loop_cache_block:
-	for (int i = 0; i < BUF_PER_PE; i++) {
+loop_cache_block:	for (int i = 0; i < BUF_PER_PE; i++) {
 #pragma HLS LOOP_FLATTEN
-		// Load data into cache directly without using an intermediate variable
-		cache_L1->entries[i].data = global_data[block_start_addr + i];
-		cache_L1->entries[i].valid = true;
-		cache_L1->entries[i].dirty = false;
-	}
+				// Load data into cache directly without using an intermediate variable
+				cache_L1->entries[i].data = global_data[block_start_addr + i];
+				cache_L1->entries[i].valid = true;
+				cache_L1->entries[i].dirty = false;
+			}
 }
 
-void buffer_load(CacheBlock cache_L1_a[PE][BUF_PER_PE], CacheBlock cache_L1_b[PE][BUF_PER_PE], u_data* global_data_a, u_data* global_data_b) {
-loop_load_outer:
-	for (int i = 0; i < PE; i++) {
-#pragma HLS LOOP_FLATTEN
-loop_load_inner:
-		for (int j = 0; j < BUF_PER_PE; j++) {
+
+void buffer_load(CacheBlock cache_L1_a[PE][BUF_PER_PE], CacheBlock cache_L1_b[PE][BUF_PER_PE], u_data* global_data_a, u_data* global_data_b, int pe_id) {
+    for (int j = 0; j < BUF_PER_PE; j++) {
+        int addr_a = pe_id * BUF_PER_PE + j;
+        int addr_b = pe_id * BUF_PER_PE + j;
+
+        if (!cache_L1_a[pe_id][j].entries[j].valid || cache_L1_a[pe_id][j].entries[j].dirty) {
+            fetch_cache_block(&cache_L1_a[pe_id][j], global_data_a, addr_a);
+            cache_L1_a[pe_id][j].entries[j].dirty = false;
+        }
+
+        if (!cache_L1_b[pe_id][j].entries[j].valid || cache_L1_b[pe_id][j].entries[j].dirty) {
+            fetch_cache_block(&cache_L1_b[pe_id][j], global_data_b, addr_b);
+            cache_L1_b[pe_id][j].entries[j].dirty = false;
+        }
+    }
+}
+
+/*
+void SSSP_kernel(u32 local_in_a[BUF_PER_PE], u32 local_in_b[BUF_PER_PE], u32 local_out[BUF_PER_PE], u32 src_vertex) {
+	CacheBlock cache_L1[BUF_PER_PE];
+#pragma HLS ARRAY_PARTITION variable=cache_L1 complete
+
+	for (int j = 0; j < BUF_PER_PE; j++) {
 #pragma HLS pipeline II=1
-			int addr_a = i * BUF_PER_PE + j;
-			int addr_b = i * BUF_PER_PE + j; // Use the same address for both caches
-			if (!cache_L1_a[i][j].entries[j].valid || cache_L1_a[i][j].entries[j].dirty) {
-				fetch_cache_block(&cache_L1_a[i][j], global_data_a, addr_a);
-				cache_L1_a[i][j].entries[j].dirty = false;
-			}
-			if (!cache_L1_b[i][j].entries[j].valid || cache_L1_b[i][j].entries[j].dirty) {
-				fetch_cache_block(&cache_L1_b[i][j], global_data_b, addr_b);
-				cache_L1_b[i][j].entries[j].dirty = false;
-			}
+		cache_L1[j].entries[j].valid = false;
+		cache_L1[j].entries[j].dirty = false;
+	}
+
+	u32 dist_buffer[BUF_PER_PE];
+#pragma HLS ARRAY_PARTITION variable=dist_buffer complete
+
+	for (int j = 0; j < BUF_PER_PE; j++) {
+#pragma HLS pipeline II=1
+		dist_buffer[j] = INF;
+	}
+
+	for (int j = 0; j < BUF_PER_PE; j++) {
+#pragma HLS pipeline II=1
+		dist_buffer[j] = (local_in_a[j] == src_vertex) ? 0 : INF;
+	}
+
+	for (int j = 0; j < BUF_PER_PE; j++) {
+#pragma HLS pipeline
+#pragma HLS LOOP_FLATTEN
+
+		u32 src = local_in_a[j]; // Edge source buffer
+		u32 dst = local_in_b[j]; // Edge destination buffer
+
+		if (dist_buffer[src] != INF && dist_buffer[src] + 1 < dist_buffer[dst]) {
+			dist_buffer[dst] = dist_buffer[src] + 1;
 		}
 	}
-}
 
 
-
-
-
-
-
-void dijkstra(CacheBlock cache_L1_a[BUF_PER_PE], CacheBlock cache_L1_b[BUF_PER_PE], u32 local_out[BUF_PER_PE], int v, int source, u_data* e_src, u_data* e_dst, u_data* out_degree) {
-	Node nodes[BUF_PER_PE];
-	for (int i = 0; i < BUF_PER_PE; i++) {
-		nodes[i].distance = INF;
-		nodes[i].visited = false;
+	for (int j = 0; j < BUF_PER_PE; j++) {
+#pragma HLS pipeline II=1
+		local_out[j] = dist_buffer[j];
 	}
+}
+*/
 
-    nodes[source].distance = 0;
+bool custom_cas(int* ptr, int expected, int desired) {
+    // Simplified CAS implementation
+    if (*ptr == expected) {
+        *ptr = desired;
+        return true;
+    }
+    return false;
+}
 
-    for (int count = 0; count < v - 1; count++) {
-        int u = -1;
-        u32 min_dist = INF;
+void SSSP_kernel(u32 local_in_a[BUF_PER_PE], u32 local_in_b[BUF_PER_PE], u32 local_out[BUF_PER_PE], int active_vertex) {
+//    int active_vertices = v; // Initialize the number of active vertices
+    int iteration = 0; // Initialize the iteration counter
 
-        // Find the unvisited node with the minimum distance
-        for (int i = 0; i < v; i++) {
-            if (!nodes[i].visited && nodes[i].distance < min_dist) {
-                u = i;
-                min_dist = nodes[i].distance;
-            }
-        }
+    int depth[BUF_PER_PE]; // Local memory to store depths
 
-        // Mark the selected node as visited
-        nodes[u].visited = true;
+    // Initialize depth array
+    for (int j = 0; j < BUF_PER_PE; j++) {
+        depth[j] = (local_in_a[j] == 0) ? 0 : INF; // Initialize depths
+    }
 
-        // Update the distance of adjacent nodes
-        for (int i = 0; i < v; i++) {
-            u32 edge_weight = 1; // Assuming edge weights are 1
-            if (!nodes[i].visited && edge_weight && nodes[u].distance != INF &&
-                nodes[u].distance + edge_weight < nodes[i].distance) {
-                nodes[i].distance = nodes[u].distance + edge_weight;
+    // Main loop
+    while (active_vertex != 0) {
+
+        active_vertex = 0; // Reset the active_vertices count
+
+        // Loop over the edges and update depths
+        for (int j = 0; j < BUF_PER_PE; j++) {
+            int target = local_in_b[j];
+            int r = depth[target];
+            int n = depth[local_in_a[j]] + 1; // Assuming e.weight is always 1
+            if (n < r) {
+                if (custom_cas(&depth[target], r, n)) {
+                    active_vertex++; // Increment active_vertices count
+                }
             }
         }
     }
 
-    // Store the shortest path distances in local_out
-    for (int i = 0; i < BUF_PER_PE; i++) {
-        local_out[i] = nodes[i].distance;
+    // Write computed depths to local_out
+    for (int j = 0; j < BUF_PER_PE; j++) {
+        local_out[j] = depth[j];
     }
 }
 
 
 
-void buffer_compute(CacheBlock cache_L1_a[PE][BUF_PER_PE], CacheBlock cache_L1_b[PE][BUF_PER_PE], u32 local_out[PE][BUF_PER_PE], int v, int source, u_data* e_src, u_data* e_dst, u_data* out_degree) {
+void buffer_compute(CacheBlock cache_L1_a[PE][BUF_PER_PE], CacheBlock cache_L1_b[PE][BUF_PER_PE], u32 local_out[PE][BUF_PER_PE], u32 active_vertex) {
+loop_compute_outer:	for (int i = 0; i < PE; i++) {
+#pragma HLS LOOP_FLATTEN//unroll
+				u32 local_in_a[BUF_PER_PE];
+#pragma HLS ARRAY_PARTITION variable=local_in_a complete
+				u32 local_in_b[BUF_PER_PE];
+#pragma HLS ARRAY_PARTITION variable=local_in_b complete
 
-	for (int pe = 0; pe < PE; pe++) {
+loop_compute_inner:	for (int j = 0; j < BUF_PER_PE; j++) {
+#pragma HLS pipeline II=1
+				local_in_a[j] = cache_L1_a[i][j].entries[j].data;
+				local_in_b[j] = cache_L1_b[i][j].entries[j].data;
+			}
+
+			SSSP_kernel(local_in_a, local_in_b, local_out[i], active_vertex);
+			}
+}
+
+void buffer_store(u_data* global_out, u32 local_out[PE][BUF_PER_PE]) {
+loop_store_outer: for (int i = 0; i < PE; i++) {
 #pragma HLS unroll
-		dijkstra(cache_L1_a[pe], cache_L1_b[pe], local_out[pe], v, source, e_src, e_dst, out_degree);
-	}
-}
-
-void buffer_store(u_data* global_out, u32 local_out[PE][BUF_PER_PE], int v) {
-loop_store_outer:
-	for (int pe = 0; pe < PE; pe++) {
-		for (int i = 0; i < BUF_PER_PE; i++) {
+loop_store_inner:	for (int j = 0; j < BUF_PER_PE; j++) {
 #pragma HLS pipeline II=1
-			int addr = pe * BUF_PER_PE + i;
-			global_out[addr] = local_out[pe][i];
-		}
-	}
+				int addr = i * BUF_PER_PE + j;
+				global_out[addr] = local_out[i][j];
+			}
+		  }
 }
 
-extern "C" {
-	void sssp_kernel_0(u_data* e_src, u_data* e_dst, u_data* out_r, int size, int vertices, u32 src_vertex, u_data* out_degree) {
-		int v = vertices;
+void sssp_kernel_0(u_data* e_src, u_data* e_dst, u_data* out_r, int size, int vertices, u32 active_vertex) {
+    CacheBlock cache_L1_a[PE][BUF_PER_PE];
+    CacheBlock cache_L1_b[PE][BUF_PER_PE];
+    u32 local_out[PE][BUF_PER_PE];
 
-		CacheBlock cache_L1_a[PE][BUF_PER_PE];
-		CacheBlock cache_L1_b[PE][BUF_PER_PE];
-#pragma HLS ARRAY_PARTITION variable=cache_L1_a complete
-#pragma HLS ARRAY_PARTITION variable=cache_L1_b complete
+#pragma HLS loop_tripcount min=1 max=PE
+    for (int i = 0; i < size / (BUF_PER_PE * PE) + 1; i++) {
+#pragma HLS unroll factor=4
+       for (int pe_id = 0; pe_id < PE; pe_id++) {
+            buffer_load(cache_L1_a, cache_L1_b, &e_src[i * BUF_PER_PE * PE], &e_dst[i * BUF_PER_PE * PE], pe_id);
+            buffer_compute(cache_L1_a, cache_L1_b, local_out, active_vertex);
+        }
 
-		u32 local_out[PE][BUF_PER_PE];
-#pragma HLS ARRAY_PARTITION variable=local_out complete
-
-		for (int i = 0; i < size / BUF_PER_PE + 1; i++) {
-			buffer_load(cache_L1_a, cache_L1_b, &e_src[i * BUF_PER_PE], &e_dst[i * BUF_PER_PE]);
-			buffer_compute(cache_L1_a, cache_L1_b, local_out, v, src_vertex, e_src, e_dst, out_degree);
-			buffer_store(&out_r[i * BUF_PER_PE], local_out, v);
-		}
-	}
+        for (int pe_id = 0; pe_id < PE; pe_id++) {
+            buffer_store(&out_r[i * BUF_PER_PE * PE], local_out);
+        }
+    }
 }
-
