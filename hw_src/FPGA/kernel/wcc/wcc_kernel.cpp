7eb767f26ce64fdd349131411f9e8be2b3f2dd6c
amin <sahebi.amin@gmail.com>
2023-09-28 10:31:36 +0100

    WCC kernel second modification based on commit on 23 Aug with loop tripcount

--- hw_src/FPGA/kernel/wcc/wcc_kernel.cpp
+++ hw_src/FPGA/kernel/wcc/wcc_kernel.cpp
@@ -8,116 +8,162 @@
 #define BUF_PER_PE 64
 
 typedef ap_uint<DATA_WIDTH> u_data;
-
 typedef unsigned int u32;
 
 struct CacheEntry {
-    u32 data;
-    bool valid;
-    bool dirty;
+	u32 data;
+	bool valid;
+	bool dirty;
 };
 
 struct CacheBlock {
-    CacheEntry entries[BUF_PER_PE];
+	CacheEntry entries[BUF_PER_PE];
 };
 
 void update_lru(CacheBlock cache[BUF_PER_PE], int index) {
-    CacheBlock temp = cache[index];
-    for (int i = index; i > 0; i--) {
+	CacheBlock temp = cache[index];
+loop_lru:	for (int i = index; i > 0; i--) {
 #pragma HLS pipeline II=1
-        cache[i] = cache[i - 1];
-    }
-    cache[0] = temp;
+			cache[i] = cache[i - 1];
+		}
+		cache[0] = temp;
 }
 
 u32 read_from_cache(CacheBlock* cache, int addr) {
-    int block_index = addr / BUF_PER_PE;
-    int block_offset = addr % BUF_PER_PE;
+	int block_index = addr / BUF_PER_PE;
+	int block_offset = addr % BUF_PER_PE;
 
-    u32 data = cache[block_index].entries[block_offset].data;
-    update_lru(cache, block_index);
+	u32 data = cache[block_index].entries[block_offset].data;
+	update_lru(cache, block_index);
 
-    return data;
+	return data;
+}
+
+void write_to_cache(CacheBlock cache[BUF_PER_PE], int addr, u32 data) {
+	cache[addr / BUF_PER_PE].entries[addr % BUF_PER_PE].data = data;
+	cache[addr / BUF_PER_PE].entries[addr % BUF_PER_PE].dirty = true;
+	update_lru(cache, addr / BUF_PER_PE);
 }
 
 void fetch_cache_block(CacheBlock* cache_L1, u_data* global_data, int addr) {
-    int block_index = addr / BUF_PER_PE;
-    int block_start_addr = block_index * BUF_PER_PE;
+	int block_index = addr / BUF_PER_PE;
+	int block_start_addr = block_index * BUF_PER_PE;
 
-    // Unroll the inner loop to reduce loop overhead
+	// Unroll the inner loop to reduce loop overhead
+loop_cache_block:	for (int i = 0; i < BUF_PER_PE; i++) {
+#pragma HLS pipeline II=1//LOOP_FLATTEN
+				// Load data into cache directly without using an intermediate variable
+				cache_L1->entries[i].data = global_data[block_start_addr + i];
+				cache_L1->entries[i].valid = true;
+				cache_L1->entries[i].dirty = false;
+			}
+}
+
+
+void buffer_load(CacheBlock cache_L1_a[PE][BUF_PER_PE], CacheBlock cache_L1_b[PE][BUF_PER_PE], u_data* global_data_a, u_data* global_data_b, int pe_id) {
+	for (int j = 0; j < BUF_PER_PE; j++) {
+#pragma HLS UNROLL//pipeline II=1
+		int addr_a = pe_id * BUF_PER_PE + j;
+		int addr_b = pe_id * BUF_PER_PE + j;
+
+		if (!cache_L1_a[pe_id][j].entries[j].valid || cache_L1_a[pe_id][j].entries[j].dirty) {
+			fetch_cache_block(&cache_L1_a[pe_id][j], global_data_a, addr_a);
+			cache_L1_a[pe_id][j].entries[j].dirty = false;
+		}
+
+		if (!cache_L1_b[pe_id][j].entries[j].valid || cache_L1_b[pe_id][j].entries[j].dirty) {
+			fetch_cache_block(&cache_L1_b[pe_id][j], global_data_b, addr_b);
+			cache_L1_b[pe_id][j].entries[j].dirty = false;
+		}
+	}
+}
+
+
+// Data structure to represent connected components
+u32 connected_components[BUF_PER_PE];
+
+// Helper function to initialize components
+void initialize_components() {
     for (int i = 0; i < BUF_PER_PE; i++) {
+        connected_components[i] = i; // Each vertex initially belongs to its own component
+    }
+}
+
+// Helper function to merge components
+void merge_components(u32 vertex_a, u32 vertex_b) {
+    u32 component_a = connected_components[vertex_a];
+    u32 component_b = connected_components[vertex_b];
+    
+    if (component_a != component_b) {
+        // Update all vertices in component_b to belong to component_a
+        for (int i = 0; i < BUF_PER_PE; i++) {
+            if (connected_components[i] == component_b) {
+                connected_components[i] = component_a;
+            }
+        }
+    }
+}
+
+// Modify the SSSP_kernel to perform WCC
+void WCC_kernel(u32 local_in_a[BUF_PER_PE], u32 local_in_b[BUF_PER_PE], u32 local_out[BUF_PER_PE]) {
+    for (int j = 0; j < BUF_PER_PE; j++) {
+        u32 src = local_in_a[j]; // Edge source buffer
+        u32 dst = local_in_b[j]; // Edge destination buffer
+
+        merge_components(src, dst); // Merge components based on edge information
+    }
+
+    // Write the connected component information to local_out
+    for (int j = 0; j < BUF_PER_PE; j++) {
+        local_out[j] = connected_components[local_in_a[j]];
+    }
+}
+
+// Modify the buffer_compute function to call WCC_kernel
+void buffer_compute(CacheBlock cache_L1_a[PE][BUF_PER_PE], CacheBlock cache_L1_b[PE][BUF_PER_PE], u32 local_out[PE][BUF_PER_PE]) {
+    for (int i = 0; i < PE; i++) {
+#pragma HLS LOOP_FLATTEN//unroll
+
+        u32 local_in_a[BUF_PER_PE];
+        u32 local_in_b[BUF_PER_PE];
+#pragma HLS ARRAY_PARTITION variable=local_in_a complete
+#pragma HLS ARRAY_PARTITION variable=local_in_b complete
+
+        for (int j = 0; j < BUF_PER_PE; j++) {
+#pragma HLS pipeline II=1
+            local_in_a[j] = cache_L1_a[i][j].entries[j].data;
+            local_in_b[j] = cache_L1_b[i][j].entries[j].data;
+        }
+
+        WCC_kernel(local_in_a, local_in_b, local_out[i]);
+    }
+}
+
+void buffer_store(u_data* global_out, u32 local_out[PE][BUF_PER_PE]) {
+loop_store_outer: for (int i = 0; i < PE; i++) {
 #pragma HLS unroll
-        // Load data into cache directly without using an intermediate variable
-        cache_L1->entries[i].data = global_data[block_start_addr + i];
-        cache_L1->entries[i].valid = true;
-        cache_L1->entries[i].dirty = false;
-    }
-}
-
-void buffer_load_compute_store(CacheBlock* cache_L1_a, CacheBlock* cache_L1_b, u_data* global_data_a, u_data* global_data_b,
-                               u32 local_out[BUF_PER_PE], int active_vertex, int start_addr) {
-    for (int j = 0; j < BUF_PER_PE; j++) {
+loop_store_inner:	for (int j = 0; j < BUF_PER_PE; j++) {
 #pragma HLS pipeline II=1
-        int addr_a = start_addr + j;
-        int addr_b = start_addr + j;
-        bool load_a = !cache_L1_a->entries[j].valid || cache_L1_a->entries[j].dirty;
-        bool load_b = !cache_L1_b->entries[j].valid || cache_L1_b->entries[j].dirty;
-
-        if (load_a) {
-            fetch_cache_block(cache_L1_a, global_data_a, addr_a);
-            cache_L1_a->entries[j].dirty = false;
-        }
-
-        if (load_b) {
-            fetch_cache_block(cache_L1_b, global_data_b, addr_b);
-            cache_L1_b->entries[j].dirty = false;
-        }
-
-        // WCC_kernel operates on a single element at a time, so the loop here is fine
-        u32 local_in_a = cache_L1_a->entries[j].data;
-        u32 local_in_b = cache_L1_b->entries[j].data;
-        int component_a = (local_in_a == active_vertex) ? addr_a : -1;
-        int component_b = (local_in_b == active_vertex) ? addr_b : -1;
-
-        // Determine the minimum component
-        int min_component = component_a;
-        if (component_b != -1 && (min_component == -1 || component_b < min_component)) {
-            min_component = component_b;
-        }
-
-        local_out[j] = min_component;
-    }
+				int addr = i * BUF_PER_PE + j;
+				global_out[addr] = local_out[i][j];
+			}
+		  }
 }
 
-void buffer_store(u_data* global_out, u32 local_out[BUF_PER_PE]) {
-    for (int j = 0; j < BUF_PER_PE; j++) {
-#pragma HLS pipeline II=1
-        global_out[j] = local_out[j];
-    }
+void wcc_kernel_0(u_data* e_src, u_data* e_dst, u_data* out_r, int size, int vertices) {
+	CacheBlock cache_L1_a[PE][BUF_PER_PE];
+	CacheBlock cache_L1_b[PE][BUF_PER_PE];
+	u32 local_out[PE][BUF_PER_PE];
+
+    initialize_components(); // Initialize connected components
+
+	for (int i = 0; i < size / (BUF_PER_PE * PE) + 1; i++) {
+#pragma HLS loop_tripcount min=1 max=PE
+		for (int pe_id = 0; pe_id < PE; pe_id++) {
+#pragma HLS unroll //factor=4
+			buffer_load(cache_L1_a, cache_L1_b, &e_src[i * BUF_PER_PE * PE], &e_dst[i * BUF_PER_PE * PE], pe_id);
+			buffer_compute(cache_L1_a, cache_L1_b, local_out);
+			buffer_store(&out_r[i * BUF_PER_PE * PE], local_out);
+		}
+	}
 }
-
-extern "C" {
-    void wcc_kernel_0(u_data* e_src, u_data* e_dst, u_data* out_r, int size, int vertices, u32 src_vertex) {
-        CacheBlock cache_L1_a[PE];
-        CacheBlock cache_L1_b[PE];
-        u32 local_out[PE][BUF_PER_PE];
-
-        int v = vertices;
-        int active_vertex = src_vertex;
-        for (int i = 0; i < size / (PE * BUF_PER_PE); i++) {
-            int start_addr = i * PE * BUF_PER_PE;
-
-            for (int pe = 0; pe < PE; pe++) {
-#pragma HLS UNROLL 
-                buffer_load_compute_store(&cache_L1_a[pe], &cache_L1_b[pe], &e_src[start_addr + pe * BUF_PER_PE],
-                                           &e_dst[start_addr + pe * BUF_PER_PE], local_out[pe], active_vertex, start_addr);
-            }
-
-            for (int pe = 0; pe < PE; pe++) {
-#pragma HLS PIPELINE II=1 
-                buffer_store(&out_r[start_addr + pe * BUF_PER_PE], local_out[pe]);
-            }
-        }
-    }
-}
-
